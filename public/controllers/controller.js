9948e7bcebb4a0683528d2c8328093826e9e0681
Sudip Baral <pujasudip@Sudips-MacBook-Pro.local>
2016-11-17 01:54:52 -0800

    mean modification

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -17,8 +17,16 @@ var refresh = function(){
 	 	$scope.data2 = "";
 	 });
  };
+ var refresh3 = function(){
+	 $http.get('/hw33').success(function(response){
+	 	console.log("I got the data I requested.");
+	 	$scope.hw33 = response;
+	 	$scope.data3 = "";
+	 });
+ };
  refresh();
  refresh2();
+ refresh3();
 
  $scope.addData = function(){
  	console.log($scope.data);
@@ -34,6 +42,13 @@ $scope.addData2 = function(){
  		refresh2();
  	});
  };
+$scope.addData3 = function(){
+ 	console.log($scope.data3);
+ 	$http.post('/hw33', $scope.data3).success(function(response){
+ 		console.log(response);
+ 		refresh3();
+ 	});
+ };
 
  $scope.remove = function(id){
  	console.log(id);
@@ -45,6 +60,12 @@ $scope.addData2 = function(){
  	console.log(id);
  	$http.delete('/hw32/' + id).success(function(response){
  		refresh2();
+ 	});
+ };
+ $scope.remove3 = function(id){
+ 	console.log(id);
+ 	$http.delete('/hw33/' + id).success(function(response){
+ 		refresh3();
  	});
  };
 
@@ -60,6 +81,12 @@ $scope.edit2 = function(id){
 		$scope.data2 = response;
 	});
 };
+$scope.edit3 = function(id){
+	console.log(id);
+	$http.get('/hw33/' + id).success(function(response){
+		$scope.data3 = response;
+	});
+};
 
 $scope.update = function(){
 	console.log($scope.data._id);
@@ -73,13 +100,21 @@ $scope.update2 = function(){
 		refresh2();
 	});
 };
-
+$scope.update3 = function(){
+	console.log($scope.data2._id);
+	$http.put('/hw33/' + $scope.data3._id, $scope.data3).success(function(response){
+		refresh3();
+	});
+};
 $scope.deselect = function(){
 	$scope.data = "";
 }
 
 $scope.deselect2 = function(){
 	$scope.data2 = "";
+}
+$scope.deselect3 = function(){
+	$scope.data3 = "";
 }
 
 }]);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('hw3', ['hw3']);
 var db2 = mongojs('hw32', ['hw32']);
+var db3 = mongojs('hw33', ['hw33']);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
@@ -92,5 +93,46 @@ app.put('/hw32/:id', function(req, res){
 	});
 });
 
+app.get('/hw33', function(req, res){
+	console.log('This is Get request.');
+
+	db3.hw33.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/hw33', function(req, res){
+	console.log(req.body);
+	db3.hw33.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.delete('/hw33/:id', function(req, res){
+	var id = req.params.id;
+	console.log(id);
+	db3.hw33.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/hw33/:id', function(req, res){
+	var id = req.params.id;
+	console.log(id);
+	db3.hw33.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+app.put('/hw33/:id', function(req, res){
+	var id = req.params.id;
+	console.log(req.body.skill);
+	db3.hw33.findAndModify({query: {_id: mongojs.ObjectId(id)},
+	update: {$set: {title: req.body.title, description: req.body.description}},
+	new: true}, function(err, doc){
+		res.json(doc);
+	});
+});
 app.listen(3000);
 console.log("Server running on port 3000");
