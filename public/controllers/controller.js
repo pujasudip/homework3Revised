35917ac65a1fc857ed424dd1e99a410267fb88ae
Sudip Baral <pujasudip@gmail.com>
2016-11-29 11:43:00 -0800

    added type functionality

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,7 +2,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-
 var refresh = function(){
 	 $http.get('/hw3').success(function(response){
 	 	console.log("I got the data I requested.");
